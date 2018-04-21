8c1b00eb48201077cda7e33a963a6b6055f239dd
willkan <wanghuikan@gmail.com>
2018-04-21 19:23:55 +0800

    starter-kit -> cli

---
+++ packages/cli/README.md
@@ -0,0 +1,17 @@
+olympic-cli
+===========
+
+
+
+[![Version](https://img.shields.io/npm/v/olympic-cli.svg)](https://npmjs.org/package/olympic-cli)
+[![CircleCI](https://circleci.com/gh/https://github.com/willkan/olympic/olympic-cli/tree/master.svg?style=shield)](https://circleci.com/gh/https://github.com/willkan/olympic/olympic-cli/tree/master)
+[![Appveyor CI](https://ci.appveyor.com/api/projects/status/github/https://github.com/willkan/olympic/olympic-cli?branch=master&svg=true)](https://ci.appveyor.com/project/https://github.com/willkan/olympic/olympic-cli/branch/master)
+[![Codecov](https://codecov.io/gh/https://github.com/willkan/olympic/olympic-cli/branch/master/graph/badge.svg)](https://codecov.io/gh/https://github.com/willkan/olympic/olympic-cli)
+[![Downloads/week](https://img.shields.io/npm/dw/olympic-cli.svg)](https://npmjs.org/package/olympic-cli)
+[![License](https://img.shields.io/npm/l/olympic-cli.svg)](https://github.com/https://github.com/willkan/olympic/olympic-cli/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ packages/cli/src/commands/create.ts
@@ -0,0 +1,43 @@
+import {Command, flags} from '@oclif/command'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import {spawn, execSync} from 'child_process'
+
+export default class Create extends Command {
+  static description = 'create project with the olympic-starter'
+
+  static examples = [
+    `$ olympic-cli create [project]
+`,
+  ]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+  }
+
+  static args = [{name: 'project', required: true}]
+
+  async run() {
+    const {args, flags} = this.parse(Create)
+    const project = args.project
+    this.log(`start to create olympic project [${project}] `)
+    const projectDir = path.join(process.cwd(), project)
+    this.log(`copying tpl to target(${projectDir})`)
+    fs.copySync(path.join(__dirname, "..", "resource", "default-tpl"), projectDir, {recursive: true})
+    this.log(`updating package.json`)
+    const packageJsonFile = path.join(projectDir, "package.json")
+    const packageObj = fs.readJsonSync(packageJsonFile) as any
+    packageObj.project = project
+    fs.writeJsonSync(packageJsonFile, packageObj)
+    this.log(`installing dependencies`)
+    let npm = 'cnpm'
+    try {
+      execSync("which cnpm")
+      this.log('use cnpm')
+    } catch (e) {
+      npm = 'npm'
+      this.log('use npm')
+    }
+    spawn(npm, ['install'], {cwd: projectDir, stdio:  [process.stdin, process.stdout, process.stderr]})
+  }
+}
