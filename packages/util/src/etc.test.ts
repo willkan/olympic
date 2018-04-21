745c5331cec91a014d9806aa700621903233d3b2
willkan <wanghuikan@gmail.com>
2018-04-21 17:59:31 +0800

    add setEnvEtcPath

--- packages/util/src/etc.test.ts
+++ packages/util/src/etc.test.ts
@@ -1,23 +1,29 @@
 import * as path from 'path'
-import {getEtc, clear, mock, setEtcDir} from './etc'
+import {getEtc, clear, mock, setEtcDir, setEnvEtcPath} from './etc'
 describe('etc', () => {
+  beforeEach(() => {
+    clear()
+    setEnvEtcPath(null)
+  })
   describe('getEtc()', () => {
     it('should only load success if config.yaml not exists', () => {
-      clear()
       setEtcDir(path.join(__dirname, 'etc-test-resource', 'etc1'))
       expect(getEtc().a).toBe(1)
     })
     it('should only load success if config.yaml exists', () => {
-      clear()
       setEtcDir(path.join(__dirname, 'etc-test-resource', 'etc2'))
       expect(getEtc().a).toBe(2)
       expect(getEtc().b.d).toBe(4)
       expect(getEtc().b).not.toHaveProperty('c')
     })
+    it('should only load success if envEtcPath exists', () => {
+      setEtcDir(path.join(__dirname, 'etc-test-resource', 'etc2'))
+      setEnvEtcPath(path.join(__dirname, 'etc-test-resource', 'etc2', 'otherConfig.yaml'))
+      expect(getEtc().a).toBe(3)
+    })
   })
   describe('mock', () => {
     it('should mock property success', () => {
-      clear()
       setEtcDir(path.join(__dirname, 'etc-test-resource', 'etc2'))
       mock({a: 3})
       expect(getEtc().a).toBe(3)
--- packages/util/src/etc.ts
+++ packages/util/src/etc.ts
@@ -4,22 +4,27 @@ import * as _ from 'lodash'
 import * as yaml from 'js-yaml'
 
 let etcDir = process.cwd()
+let envEtcPath
 
 export function setEtcDir(_etcDir: string) {
   etcDir = _etcDir
+}
+
+export function setEnvEtcPath(_envEtcPath: string) {
+  envEtcPath = _envEtcPath
 }
 
 let etc
 
 function init() {
   let defaultPath = path.join(etcDir, "config.default.yaml")
-  let envPath = path.join(etcDir, "config.yaml")
+  const _envEtcPath = envEtcPath == void 0 ? envEtcPath = path.join(etcDir, "config.yaml") : envEtcPath
   let defaultEtc = yaml.safeLoad(fs.readFileSync(defaultPath, 'utf8'))
   let envEtc
-  if (fs.existsSync(envPath)) {
-    envEtc = yaml.safeLoad(fs.readFileSync(envPath, 'utf8'))
+  if (fs.existsSync(_envEtcPath)) {
+    envEtc = yaml.safeLoad(fs.readFileSync(_envEtcPath, 'utf8'))
   } else {
-    console.warn('cannot found config.yaml, it will only use config.default.yaml as etc')
+    console.warn(`cannot found ${_envEtcPath}, it will only use config.default.yaml as etc`)
   }
   etc = _.extend({
     root: path.join(etcDir)
